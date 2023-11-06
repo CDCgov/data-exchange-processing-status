a73a56de3a334f5b1e6fe8b18f2b9e85aea915c7
Matt B Krystof <ygj6@cdc.gov>
2023-11-06 14:19:33 -0600

    Trigger and gradle updates

--- processing-status-api-function-app/src/main/kotlin/gov/cdc/ocio/processingstatusapi/functions/FunctionTriggers.kt
+++ processing-status-api-function-app/src/main/kotlin/gov/cdc/ocio/processingstatusapi/functions/FunctionTriggers.kt
@@ -8,32 +8,50 @@ import com.microsoft.azure.functions.annotation.AuthorizationLevel
 import com.microsoft.azure.functions.annotation.BindingName
 import com.microsoft.azure.functions.annotation.FunctionName
 import com.microsoft.azure.functions.annotation.HttpTrigger
-import java.util.*
+import java.util.Optional
 
 class FunctionTriggers {
 
     @FunctionName("HealthCheck")
     fun healthCheck(
-            @HttpTrigger(name = "req", methods = [HttpMethod.GET], route = "status/health", authLevel = AuthorizationLevel.ANONYMOUS) request: HttpRequestMessage<Optional<String>>,
-            context: ExecutionContext?): HttpResponseMessage {
-        return HealthCheckFunction().run(request, context!!)
+            @HttpTrigger(
+                    name = "req",
+                    methods = [HttpMethod.GET],
+                    route = "status/health",
+                    authLevel = AuthorizationLevel.ANONYMOUS
+            ) request: HttpRequestMessage<Optional<String>>,
+            context: ExecutionContext
+    ): HttpResponseMessage {
+        return HealthCheckFunction().run(request, context)
     }
 
     @FunctionName("Trace")
     fun trace(
-            @HttpTrigger(name = "req", methods = [HttpMethod.POST], route = "trace/{spanName}", authLevel = AuthorizationLevel.ANONYMOUS) request: HttpRequestMessage<Optional<String>>,
-            @BindingName("spanName") spanName: String?,
-            context: ExecutionContext?): HttpResponseMessage {
-        return TraceFunction().run(request, spanName!!, context!!)
+            @HttpTrigger(
+                    name = "req",
+                    methods = [HttpMethod.POST],
+                    route = "trace/{spanName}",
+                    authLevel = AuthorizationLevel.ANONYMOUS
+            ) request: HttpRequestMessage<Optional<String>>,
+            @BindingName("spanName") spanName: String,
+            context: ExecutionContext
+    ): HttpResponseMessage {
+        return TraceFunction().run(request, spanName, context)
     }
 
     @FunctionName("AddSpanToTrace")
     fun addSpanToTrace(
-            @HttpTrigger(name = "req", methods = [HttpMethod.PUT], route = "span/{traceId}/{spanId}/{spanName}", authLevel = AuthorizationLevel.ANONYMOUS) request: HttpRequestMessage<Optional<String>>,
-            @BindingName("traceId") traceId: String?,
-            @BindingName("spanId") spanId: String?,
-            @BindingName("spanName") spanName: String?,
-            context: ExecutionContext?): HttpResponseMessage {
-        return AddSpanToTraceFunction().run(request, traceId!!, spanId!!, spanName!!, context!!)
+            @HttpTrigger(
+                    name = "req",
+                    methods = [HttpMethod.PUT],
+                    route = "span/{traceId}/{spanId}/{spanName}",
+                    authLevel = AuthorizationLevel.ANONYMOUS
+            ) request: HttpRequestMessage<Optional<String>>,
+            @BindingName("traceId") traceId: String,
+            @BindingName("spanId") spanId: String,
+            @BindingName("spanName") spanName: String,
+            context: ExecutionContext
+    ): HttpResponseMessage {
+        return AddSpanToTraceFunction().run(request, traceId, spanId, spanName, context)
     }
 }
