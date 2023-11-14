5c1dff68f7c5f2726490bdda26c9edc4eb577337
Surya Talla <124595803+uil9@users.noreply.github.com>
2023-11-14 08:59:52 -0500

    DASB-87 & 88 - Implemented /trace and /addSpan endpoints (#1)
    * DASB-87 & 88 - Implemented /trace and /addSpan endpoints
    
    * DASB-87 & 88 - Added Headers for Class and methods

---
+++ processing-status-api-function-app/src/main/kotlin/gov/cdc/ocio/processingstatusapi/Constants.kt
@@ -0,0 +1,5 @@
+package gov.cdc.ocio.processingstatusapi
+
+object Constants {
+    const val PARENT_SPAN = "PARENT_SPAN"
+}
--- processing-status-api-function-app/src/main/kotlin/gov/cdc/ocio/processingstatusapi/functions/AddSpanToTraceFunction.kt
+++ processing-status-api-function-app/src/main/kotlin/gov/cdc/ocio/processingstatusapi/functions/AddSpanToTraceFunction.kt
@@ -10,26 +10,39 @@ import io.opentelemetry.context.Context
 import java.util.*
 
 
+/**
+ * Create a processing status span for a given trace
+ *
+ */
 class AddSpanToTraceFunction {
 
     private var tracer: Tracer? = null
 
+    /**
+     * For a given HTTP request, this method creates a processing status span for a given trace.
+     * In order to process, the HTTP request must contain stageName and spanMark.
+     * @param request HttpRequestMessage<Optional<String>>
+     * @param traceId String
+     * @param spanId String
+     * @param context ExecutionContext
+     * @return HttpResponseMessage - resultant HTTP response message for the given request
+     */
     fun run(
         request: HttpRequestMessage<Optional<String>>,
         traceId: String,
         spanId: String,
-        spanName: String,
         context: ExecutionContext
     ): HttpResponseMessage {
 
         val logger = context.logger
-
-        val jaegerEndpoint = "http://ocioededevjaeger.eastus.azurecontainer.io:4317"
-        val openTelemetry = OpenTelemetryConfig.initOpenTelemetry(jaegerEndpoint)
+        val openTelemetry = OpenTelemetryConfig.initOpenTelemetry()
 
         logger.info("HTTP trigger processed a ${request.httpMethod.name} request.")
-        logger.info("span name = $spanName")
-
+        val stageName = request.queryParameters["stageName"]
+        val spanMark = request.queryParameters["spanMark"]
+        logger.info("StageName: $stageName")
+        logger.info("SpanMark: $spanMark")
+        logger.info(spanMark)
         tracer = openTelemetry.getTracer(AddSpanToTraceFunction::class.java.name)
 
         val spanContext = SpanContext.createFromRemoteParent(
@@ -39,27 +52,32 @@ class AddSpanToTraceFunction {
             TraceState.getDefault()
         )
 
-        val span = tracer!!.spanBuilder(spanName)
-            .setParent(Context.current().with(Span.wrap(spanContext)))
-            .startSpan()
-        try {
-            span.setAttribute("some_field", UUID.randomUUID().toString())
-            span.makeCurrent().use { ignored ->
-                Thread.sleep(500)
-            }
-        } finally {
+        if(spanMark!= null && (spanMark == "start" || spanMark == "stop")) {
+            tracer = openTelemetry.getTracer(TraceFunction::class.java.name)
+
+            val span = tracer!!.spanBuilder(stageName)
+                .setParent(Context.current().with(Span.wrap(spanContext)))
+                .startSpan()
+            span.setAttribute("spanMark", spanMark)
             span.end()
+
+            val result = TraceResult()
+            result.traceId = traceId
+            result.spanId = spanId
+            return request
+                .createResponseBuilder(HttpStatus.OK)
+                .header("Content-Type", "application/json")
+                .body(result)
+                .build()
+
+        } else{
+            return request
+                .createResponseBuilder(HttpStatus.BAD_REQUEST)
+                .header("Content-Type", "application/json")
+                .body("The provided spanMark is not allowed.")
+                .build()
         }
 
-        val result = TraceResult()
-        result.status = "OK"
-        result.traceId = span.spanContext.traceId
-
-        return request
-            .createResponseBuilder(HttpStatus.OK)
-            .header("Content-Type", "application/json")
-            .body(result)
-            .build()
     }
 
 }
