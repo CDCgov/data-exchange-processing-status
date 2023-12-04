f3eea577f701aee4fe7f37996389e356f2d59896
Surya Talla <124595803+uil9@users.noreply.github.com>
2023-12-04 11:00:06 -0500

    DASB-144 - Revamp Processing Status API health endpoints (#12)

--- processing-status-api-function-app/src/main/kotlin/gov/cdc/ocio/processingstatusapi/functions/HealthCheckFunction.kt
+++ processing-status-api-function-app/src/main/kotlin/gov/cdc/ocio/processingstatusapi/functions/HealthCheckFunction.kt
@@ -1,22 +1,86 @@
 package gov.cdc.ocio.processingstatusapi.functions
 
+import com.azure.cosmos.models.CosmosQueryRequestOptions
 import com.microsoft.azure.functions.ExecutionContext
 import com.microsoft.azure.functions.HttpRequestMessage
 import com.microsoft.azure.functions.HttpResponseMessage
 import com.microsoft.azure.functions.HttpStatus
+import com.microsoft.azure.servicebus.QueueClient
+import com.microsoft.azure.servicebus.ReceiveMode
+import com.microsoft.azure.servicebus.primitives.ConnectionStringBuilder
+import gov.cdc.ocio.cosmossync.cosmos.CosmosClientManager.Companion.getCosmosClient
+import gov.cdc.ocio.processingstatusapi.model.CosmosDb
+import gov.cdc.ocio.processingstatusapi.model.HealthCheck
+import gov.cdc.ocio.processingstatusapi.model.Report
+import gov.cdc.ocio.processingstatusapi.model.ServiceBus
 import java.util.*
+import kotlin.system.measureTimeMillis
 
 class HealthCheckFunction {
 
     fun run(
         request: HttpRequestMessage<Optional<String>>,
-        context: ExecutionContext
+        context: ExecutionContext,
     ): HttpResponseMessage {
-    
-        // todo
+        val logger = context.logger
+        val result = HealthCheck()
+        var cosmosDBHealthy = false;
+        var serviceBusHealthy = false;
+        var cosmosDBHealth = CosmosDb()
+        var serviceBusHealth = ServiceBus()
+        val time = measureTimeMillis {
+            try {
+                cosmosDBHealthy = isCosmosDBHealthy()
+                cosmosDBHealth.status = "UP"
+            } catch (ex: Exception) {
+                cosmosDBHealth.health_issues(ex.message)
+                logger.warning("CosmosDB is not healthy: ${ex.message}")
+            }
 
+            try {
+                serviceBusHealthy = isServiceBusHealthy()
+                serviceBusHealth.status = "UP"
+            } catch (ex: Exception) {
+                serviceBusHealth.health_issues(ex.message)
+                logger.warning("Azure Service Bus is not healthy: ${ex.message}")
+            }
+        }
+
+        if(cosmosDBHealthy && serviceBusHealthy){
+            result.status = "UP"
+        }
+        result.total_checks_duration(time)
+        result.dependency_health_checks.add(cosmosDBHealth)
+        result.dependency_health_checks.add(serviceBusHealth)
         return request
             .createResponseBuilder(HttpStatus.OK)
+            .header("Content-Type", "application/json")
+            .body(result)
             .build()
     }
+
+    private fun isCosmosDBHealthy(): Boolean {
+        val databaseName = System.getenv("CosmosDbDatabaseName")
+        val containerName = System.getenv("CosmosDbContainerName")
+
+        val cosmosDB = getCosmosClient().getDatabase(databaseName)
+        val container = cosmosDB.getContainer(containerName)
+
+        val sqlQuery = "select * from $containerName t OFFSET 0 LIMIT 1"
+        val items = container.queryItems(
+            sqlQuery, CosmosQueryRequestOptions(),
+            Report::class.java
+        )
+        return true
+    }
+
+    private fun isServiceBusHealthy():Boolean {
+        val connectionString = System.getenv("ServiceBusConnectionString")
+        val queueName = System.getenv("ServiceBusQueueName")
+        val queueClient = QueueClient(ConnectionStringBuilder(connectionString, queueName), ReceiveMode.PEEKLOCK)
+        queueClient.close()
+        return true;
+    }
+
+
 }
