334c15313e6fa49956098e6ffc01b0276a9ee4a7
Matt B Krystof <mattkrystof@gmail.com>
2023-12-11 13:56:39 -0600

    Send a trace for upload with the load testing script

--- processing-status-api-function-app/test/load-testing/pstatus_load_test.py
+++ processing-status-api-function-app/test/load-testing/pstatus_load_test.py
@@ -1,7 +1,9 @@
 import asyncio
 import uuid
 import time
+import requests
 import reports
+import json
 from azure.servicebus.aio import ServiceBusClient
 from azure.servicebus import ServiceBusMessage
 from azure.servicebus import TransportType
@@ -38,6 +40,8 @@ async def run():
     conn_str1=env["service_bus_connection_str"]
     print("sb str = " + conn_str1)
 
+    pstatus_base_url=env["pstatus_api_base_url"]
+
     # create a Service Bus client using the connection string
     async with ServiceBusClient.from_connection_string(
         conn_str=env["service_bus_connection_str"],
@@ -46,16 +50,26 @@ async def run():
         # Get a Queue Sender object to send messages to the queue
         sender = servicebus_client.get_queue_sender(queue_name=QUEUE_NAME)
         async with sender:
+            # Create the trace for this upload
+            url = f'{pstatus_base_url}/api/trace?uploadId={upload_id}&destinationId=dex-testing&eventType=test-event1'
+            response = requests.post(url)
+            response_json = response.json()
+            trace_id = response_json["trace_id"]
+            parent_span_id = response_json["span_id"]
+            url = f'{pstatus_base_url}/api/trace/addSpan/{trace_id}/{parent_span_id}?stageName=dex-upload&spanMark=start'
+            requests.put(url)
             # Send upload messages
             print("Sending simulated UPLOAD reports...")
             num_chunks = 3
             size = 27472691
             for index in range(num_chunks):
-                offset = (index+1) * size / num_chunks 
+                offset = (index+1) * size / num_chunks
                 message = reports.create_upload(upload_id, offset, size)
                 #print(f"Sending: {message}")
                 await send_single_message(sender, message)
                 time.sleep(1)
+            url = f'{pstatus_base_url}/api/trace/addSpan/{trace_id}/{parent_span_id}?stageName=dex-upload&spanMark=stop'
+            requests.put(url)
             # Send routing message
             print("Sending simulated ROUTING report...")
             message = reports.create_routing(upload_id)
@@ -64,7 +78,7 @@ async def run():
 
             # Send hl7 validation messages
             print("Sending simulated HL7-VALIDATION reports...")
-            num_lines = 100
+            num_lines = 2
             for index in range(num_lines):
                 line = index + 1
                 message = reports.create_hl7_validation(upload_id, line)
