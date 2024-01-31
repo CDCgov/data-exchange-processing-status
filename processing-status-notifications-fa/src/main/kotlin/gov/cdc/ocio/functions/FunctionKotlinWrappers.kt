5ea782d660fa7acebe8d76f23fd71765e10aed61
Nagpal <rxv5@cdc.gov>
2024-01-31 11:49:55 -0500

    Removing methods for Service bus

--- processing-status-notifications-fa/src/main/kotlin/gov/cdc/ocio/functions/FunctionKotlinWrappers.kt
+++ processing-status-notifications-fa/src/main/kotlin/gov/cdc/ocio/functions/FunctionKotlinWrappers.kt
@@ -4,7 +4,6 @@ import com.microsoft.azure.functions.annotation.*
 import gov.cdc.ocio.functions.http.SubscribeEmailNotifications
 import gov.cdc.ocio.functions.http.SubscribeWebsocketNotifications
 import gov.cdc.ocio.functions.http.UnsubscribeNotifications
-import gov.cdc.ocio.functions.servicebus.EmailServiceBusProcessor
 import java.util.*
 
 class FunctionKotlinWrappers {
@@ -57,24 +56,5 @@ class FunctionKotlinWrappers {
         context.logger.info("HTTP trigger processed a ${request.httpMethod.name} request.")
         return UnsubscribeNotifications().run(request, notificationType, subscriptionId, context);
     }
-
-    // Service Bus Queue Triggers for Message Calls
-    @FunctionName("EmailServiceBusProcessor")
-    fun emailServiceBusProcessor(
-        @ServiceBusQueueTrigger(
-            name = "msg",
-            queueName = "%ServiceBusQueueName%",
-            connection = "ServiceBusConnectionString"
-        ) message: String,
-        context: ExecutionContext
-    ) {
-        try {
-            context.logger.info("Received message: $message")
-            EmailServiceBusProcessor(context).withMessage(message)
-        } catch (e: Exception) {
-            context.logger.warning("Failed to process service bus message: " + e.localizedMessage)
-        }
-    }
-
 
 }
