18b57707926dcb75e69013524645f91995fb657a
uil9 <uil9@cdc.gov>
2024-03-07 18:20:39 -0500

    Replaced stage_report_id to report_id

--- processing-status-api-function-app/src/main/kotlin/gov/cdc/ocio/processingstatusapi/model/reports/CreateReportResult.kt
+++ processing-status-api-function-app/src/main/kotlin/gov/cdc/ocio/processingstatusapi/model/reports/CreateReportResult.kt
@@ -5,9 +5,9 @@ import com.google.gson.annotations.SerializedName
 /**
  * Contains all the properties for the return response when a report has been successfully amended.
  *
- * @property stageReportId String
+ * @property reportId String
  * @property stageName String
  * @constructor
  */
-data class CreateReportResult(@SerializedName("stage_report_id") val stageReportId: String,
+data class CreateReportResult(@SerializedName("report_id") val reportId: String,
                               @SerializedName("stage_name") val stageName: String)
