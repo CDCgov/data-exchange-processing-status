b1a6fe20292d8f170f568476570870ed2457826b
Matt B Krystof <ygj6@cdc.gov>
2024-05-21 21:08:58 -0500

    Initial stubs for auth

--- pstatus-graphql-ktor/src/main/kotlin/gov/cdc/ocio/processingstatusapi/plugins/GraphQL.kt
+++ pstatus-graphql-ktor/src/main/kotlin/gov/cdc/ocio/processingstatusapi/plugins/GraphQL.kt
@@ -1,5 +1,7 @@
 package gov.cdc.ocio.processingstatusapi.plugins
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import com.expediagroup.graphql.dataloader.KotlinDataLoaderRegistryFactory
 import com.expediagroup.graphql.server.ktor.*
 import gov.cdc.ocio.processingstatusapi.dataloaders.ReportDataLoader
@@ -10,6 +12,8 @@ import gov.cdc.ocio.processingstatusapi.subscriptions.ErrorSubscriptionService
 import io.ktor.http.*
 import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -39,6 +43,33 @@ fun Application.graphQLModule() {
         pingPeriod = Duration.ofSeconds(1)
         contentConverter = JacksonWebsocketContentConverter()
     }
+    // see https://ktor.io/docs/server-jwt.html#configure-verifier
+    val secret = environment.config.property("jwt.secret").getString()
+    val issuer = environment.config.property("jwt.issuer").getString()
+    val audience = environment.config.property("jwt.audience").getString()
+    val myRealm = environment.config.property("jwt.realm").getString()
+    install(Authentication) {
+        jwt {
+            jwt("auth-jwt") {
+                realm = myRealm
+                verifier(JWT
+                    .require(Algorithm.HMAC256(secret))
+                    .withAudience(audience)
+                    .withIssuer(issuer)
+                    .build())
+                validate { credential ->
+                    if (credential.payload.getClaim("username").asString() != "") {
+                        JWTPrincipal(credential.payload)
+                    } else {
+                        null
+                    }
+                }
+                challenge { _, _ ->
+                    call.respond(HttpStatusCode.Unauthorized, "Token is not valid or has expired")
+                }
+            }
+        }
+    }
     install(StatusPages) {
         defaultGraphQLStatusPages()
     }
@@ -64,6 +95,14 @@ fun Application.graphQLModule() {
         }
     }
     install(Routing) {
+        authenticate("auth-jwt") {
+//            post("/graphql") {
+//                val principal = call.principal<JWTPrincipal>()
+//                val username = principal!!.payload.getClaim("username").asString()
+//                val expiresAt = principal.expiresAt?.time?.minus(System.currentTimeMillis())
+//                call.respondText("Hello, $username! Token is expired at $expiresAt ms.")
+//            }
+        }
         graphQLPostRoute()
         graphQLSubscriptionsRoute()
         graphQLSDLRoute()
