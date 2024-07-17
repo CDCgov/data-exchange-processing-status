4efb8a9d679df52c6a3f33f38565f958759aa209
Matt B Krystof <ygj6@cdc.gov>
2024-07-17 13:48:02 -0500

    Added missing headers from report loader.

--- pstatus-graphql-ktor/src/main/kotlin/gov/cdc/ocio/processingstatusapi/queries/ReportQueryService.kt
+++ pstatus-graphql-ktor/src/main/kotlin/gov/cdc/ocio/processingstatusapi/queries/ReportQueryService.kt
@@ -10,11 +10,25 @@ import java.util.concurrent.CompletableFuture
 
 class ReportQueryService : Query {
 
+    /**
+     * Query for retrieving reports for the uploadId provided.
+     *
+     * @param dataFetchingEnvironment DataFetchingEnvironment
+     * @param uploadId String
+     * @return List<Report>
+     */
     @GraphQLDescription("Return all the reports associated with the provided uploadId")
     @Suppress("unused")
     fun getReports(dataFetchingEnvironment: DataFetchingEnvironment,
                    uploadId: String) = ReportLoader().getByUploadId(dataFetchingEnvironment, uploadId)
 
+    /**
+     * Searches for reports with the given search parameters.
+     *
+     * @param params ReportSearchParameters
+     * @param dfe DataFetchingEnvironment
+     * @return CompletableFuture<List<Report>>
+     */
     @GraphQLDescription("Return list of reports based on ReportSearchParameters options")
     @Suppress("unused")
     fun searchReports(params: ReportSearchParameters, dfe: DataFetchingEnvironment): CompletableFuture<List<Report>> =
@@ -22,6 +36,12 @@ class ReportQueryService : Query {
             .loadMany(params.ids)
 }
 
+/**
+ * Report search parameters, which is a list of report IDs.
+ *
+ * @property ids List<String>
+ * @constructor
+ */
 @GraphQLDescription("Parameters for searching for reports")
 data class ReportSearchParameters(
     @GraphQLDescription("Array of report IDs to search for and retrieve")
