93bf92cac5ee923df515ada06200fc546972fae5
Manu Kesava <xph6@cdc.gov>
2024-08-07 17:31:56 -0400

    PoC for EasyRules

--- spikes/EasyRulesEngine/src/main/kotlin/rulesEngine/data/RuleData.kt
+++
@@ -1,5 +0,0 @@
-import java.time.LocalTime
-
-data class Jurisdiction(val name: String, val lastUploadTime: LocalTime?)
-data class UploadStatus(val completed: Boolean, val hasErrors: Boolean)
-data class DataStream(val name: String, val uploadCount: Int)
