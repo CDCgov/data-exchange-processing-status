4cc564662416264b5632e54b6765eada57d96b17
Manu Kesava <xph6@cdc.gov>
2024-08-19 16:00:56 -0400

    First commit of the PoC for temporal workflow orchestrator

---
+++ spikes/temporal-workflow-orchestrator-poc/src/main/kotlin/Main.kt
@@ -0,0 +1,38 @@
+import io.temporal.client.WorkflowClient
+import io.temporal.client.WorkflowOptions
+import io.temporal.serviceclient.WorkflowServiceStubs
+import io.temporal.worker.WorkerFactory
+
+fun main() {
+    val service = WorkflowServiceStubs.newLocalServiceStubs()
+    val client = WorkflowClient.newInstance(service)
+    val factory = WorkerFactory.newInstance(client)
+    val taskQueue = "notificationTaskQueue"
+
+    val worker = factory.newWorker(taskQueue)
+    worker.registerWorkflowImplementationTypes(NotificationWorkflowImpl::class.java)
+    worker.registerActivitiesImplementations(NotificationActivitiesImpl())
+
+    factory.start()
+
+    val workflowOptions = WorkflowOptions.newBuilder()
+        .setTaskQueue(taskQueue)
+        .build()
+
+    val workflow = client.newWorkflowStub(
+        NotificationWorkflow::class.java,
+        workflowOptions
+    )
+
+    val daysToRun = listOf("MO", "Tu", "We", "Th", "Fr")
+    val timeToRun = "19:54:10+00:00"
+
+    workflow.checkUploadAndNotify(
+        dataStreamId = "dataStreamId",
+        dataStreamRoute = "dataStreamRoute",
+        jurisdiction = "jurisdiction",
+        daysToRun = daysToRun,
+        timeToRun = timeToRun,
+        deliveryReference = "xph6@cdc.gov"
+    )
+}
