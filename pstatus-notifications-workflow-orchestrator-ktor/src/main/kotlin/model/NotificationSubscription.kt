cc27438e949a57177bf680f1604cb64a77ca0ae7
Manu Kesava <xph6@cdc.gov>
2024-09-11 09:40:34 -0400

    Promoting the temporal workflow orchestrator to its own microservice

---
+++ pstatus-notifications-workflow-orchestrator-ktor/src/main/kotlin/model/NotificationSubscription.kt
@@ -0,0 +1,11 @@
+package gov.cdc.ocio.processingnotifications.model
+/**
+ * Notification subscription response class
+ * @param subscriptionId String
+ * @param subscription BaseSubscription
+ */
+
+class NotificationSubscriptionResponse(val subscriptionId: String,
+                               val  subscription: BaseSubscription)
+
+
