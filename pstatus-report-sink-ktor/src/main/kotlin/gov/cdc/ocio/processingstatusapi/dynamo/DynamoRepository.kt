04029b3bb7793835081cb060e54067b098229d0b
Matt B Krystof <ygj6@cdc.gov>
2024-09-13 15:08:30 -0500

    Make DynamoDbClient accessible from repository

--- pstatus-report-sink-ktor/src/main/kotlin/gov/cdc/ocio/processingstatusapi/dynamo/DynamoRepository.kt
+++ pstatus-report-sink-ktor/src/main/kotlin/gov/cdc/ocio/processingstatusapi/dynamo/DynamoRepository.kt
@@ -9,9 +9,6 @@ import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsPro
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
-import software.amazon.awssdk.services.dynamodb.model.DynamoDbException
-import software.amazon.awssdk.services.dynamodb.model.ListTablesRequest
-import software.amazon.awssdk.services.dynamodb.model.ListTablesResponse
 
 
 /**
@@ -26,67 +23,40 @@ import software.amazon.awssdk.services.dynamodb.model.ListTablesResponse
  */
 class DynamoRepository(dbPrefix: String): ProcessingStatusRepository() {
 
+    private val ddbClient = getDynamoDbClient()
+
     private val ddbEnhancedClient = getDynamoDbEnhancedClient()
 
     private val reportsTableName = "$dbPrefix-reports".lowercase()
 
     private val reportsDeadLetterTableName = "$dbPrefix-reports-deadletter".lowercase()
 
-    override var reportsCollection = DynamoCollection(ddbEnhancedClient, reportsTableName, Report::class.java) as Collection
+    override var reportsCollection =
+        DynamoCollection(
+            ddbClient,
+            ddbEnhancedClient,
+            reportsTableName,
+            Report::class.java
+        ) as Collection
 
-    override var reportsDeadLetterCollection = DynamoCollection(ddbEnhancedClient, reportsDeadLetterTableName, ReportDeadLetter::class.java) as Collection
+    override var reportsDeadLetterCollection = DynamoCollection(
+        ddbClient,
+        ddbEnhancedClient,
+        reportsDeadLetterTableName,
+        ReportDeadLetter::class.java
+    ) as Collection
 
-    private fun getDynamoDbEnhancedClient(): DynamoDbEnhancedClient {
-
+    private fun getDynamoDbClient() : DynamoDbClient {
         // Load credentials from the AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY and AWS_SESSION_TOKEN environment variables.
-        val ddb = DynamoDbClient.builder()
+        return DynamoDbClient.builder()
             .region(Region.US_EAST_1)
             .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
             .build()
+    }
 
-        listAllTables(ddb)
-
+    private fun getDynamoDbEnhancedClient(): DynamoDbEnhancedClient {
         return DynamoDbEnhancedClient.builder()
-            .dynamoDbClient(ddb)
+            .dynamoDbClient(ddbClient)
             .build()
     }
-
-    fun listAllTables(ddb: DynamoDbClient) {
-        var moreTables = true
-        var lastName: String? = null
-
-        while (moreTables) {
-            try {
-                var response: ListTablesResponse? = null
-                if (lastName == null) {
-                    val request = ListTablesRequest.builder().build()
-                    response = ddb.listTables(request)
-                } else {
-                    val request = ListTablesRequest.builder()
-                        .exclusiveStartTableName(lastName).build()
-                    response = ddb.listTables(request)
-                }
-
-                val tableNames = response.tableNames()
-                if (tableNames.size > 0) {
-                    for (curName in tableNames) {
-                        System.out.format("* %s\n", curName)
-                    }
-                } else {
-                    println("No tables found!")
-                    System.exit(0)
-                }
-
-                lastName = response.lastEvaluatedTableName()
-                if (lastName == null) {
-                    moreTables = false
-                }
-            } catch (e: DynamoDbException) {
-                System.err.println(e.message)
-                System.exit(1)
-            }
-        }
-        println("\nDone!")
-    }
-
 }
