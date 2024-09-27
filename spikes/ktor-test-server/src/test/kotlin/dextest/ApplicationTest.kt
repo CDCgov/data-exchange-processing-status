013a82b209fa60aa278fd04ba4ad5d3c89d4fe82
ZacharyRizer <zacharyrizer@gmail.com>
2024-09-27 12:40:04 -0400

    Added root level test

---
+++ spikes/ktor-test-server/src/test/kotlin/dextest/ApplicationTest.kt
@@ -0,0 +1,24 @@
+package dextest
+
+import dextest.plugins.configureAuth
+import dextest.plugins.configureRouting
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import kotlin.test.*
+
+class ApplicationTest {
+
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureAuth()
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+    }
+}
