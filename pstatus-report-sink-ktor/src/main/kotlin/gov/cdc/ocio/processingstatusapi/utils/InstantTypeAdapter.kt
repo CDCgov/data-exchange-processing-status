fa25a5b92cd36acb6f6b7aabcabbcf712784ad5e
Matt B Krystof <ygj6@cdc.gov>
2024-09-29 17:03:52 -0500

    Removed unneeded class from report-sink, added missing comment headers

--- pstatus-report-sink-ktor/src/main/kotlin/gov/cdc/ocio/processingstatusapi/utils/InstantTypeAdapter.kt
+++ pstatus-report-sink-ktor/src/main/kotlin/gov/cdc/ocio/processingstatusapi/utils/InstantTypeAdapter.kt
@@ -5,14 +5,33 @@ import java.lang.reflect.Type
 import java.time.Instant
 
 
+/**
+ * Instant type adaptor used by gson
+ */
 class InstantTypeAdapter :
     JsonSerializer<Instant?>,
     JsonDeserializer<Instant?>
 {
+    /**
+     * Serialize implementation for the Instant type.
+     *
+     * @param date Instant?
+     * @param typeOfSrc Type
+     * @param context JsonSerializationContext
+     * @return JsonElement
+     */
     override fun serialize(date: Instant?, typeOfSrc: Type?, context: JsonSerializationContext?): JsonElement {
         return JsonPrimitive(date?.toEpochMilli())
     }
 
+    /**
+     * Deserialize implemenation for the Instant type.
+     *
+     * @param json JsonElement
+     * @param typeOfT Type
+     * @param context JsonDeserializationContext
+     * @return Instant?
+     */
     override fun deserialize(json: JsonElement?, typeOfT: Type?, context: JsonDeserializationContext?): Instant? {
         return json?.asString?.let { Instant.parse(it) }
     }
