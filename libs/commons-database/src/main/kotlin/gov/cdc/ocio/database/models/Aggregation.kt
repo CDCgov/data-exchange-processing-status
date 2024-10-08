9233d219d7e8200350597fdaf205d025a521f3e4
Matt B Krystof <ygj6@cdc.gov>
2024-10-08 09:01:19 -0500

    Fixed aggregation enum comment

--- libs/commons-database/src/main/kotlin/gov/cdc/ocio/database/models/Aggregation.kt
+++ libs/commons-database/src/main/kotlin/gov/cdc/ocio/database/models/Aggregation.kt
@@ -3,7 +3,8 @@ package gov.cdc.ocio.database.models
 import com.google.gson.annotations.SerializedName
 
 /**
- * Issue leve; of Report-ERROR OR WARNING
+ * Indicated the type of aggregation for the report.  Single indicates the upload maps to only one item whereas batch
+ * means the upload maps to multiple items.
  */
 enum class Aggregation {
     @SerializedName("SINGLE")
