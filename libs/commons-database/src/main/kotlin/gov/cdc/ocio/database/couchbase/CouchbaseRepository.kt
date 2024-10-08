59d9cf369cd20ac43ba926ba5f21b09ff11ef9d7
Matt B Krystof <ygj6@cdc.gov>
2024-10-08 09:47:21 -0500

    Added bucket name, scope name, and report collection names as params to the c'tor of couchbase

--- libs/commons-database/src/main/kotlin/gov/cdc/ocio/database/couchbase/CouchbaseRepository.kt
+++ libs/commons-database/src/main/kotlin/gov/cdc/ocio/database/couchbase/CouchbaseRepository.kt
@@ -13,13 +13,16 @@ import java.time.Duration
  * @param connectionString[String] Connection string for the couchbase database.
  * @param username[String] Username to use when connecting to the database.
  * @param password[String] Password to use when connecting to the database.
+ * @param bucketName[String] Name of the bucket within the cluster to use.
+ * @param scopeName[String] Name of the scope for the couchbase collections.
+ * @param reportsCollectionName[String] Reports collection name to use, which defaults to "Reports" if not provided.
+ * @param reportsDeadLetterCollectionName[String] Reports deadletter collection name to use, which default to
+ * "Reports-DeadLetter" if not provided.
  * @property cluster (Cluster..Cluster?)
  * @property processingStatusBucket (Bucket..Bucket?)
  * @property scope Scope
  * @property reportsCouchbaseCollection Collection
  * @property reportsDeadLetterCouchbaseCollection Collection
- * @property reportsCollectionName String
- * @property reportsDeadLetterCollectionName String
  * @property reportsCollection Collection
  * @property reportsDeadLetterCollection Collection
  * @constructor Provides a Couchbase repository, which is a concrete implementation of the [ProcessingStatusRepository]
@@ -29,13 +32,17 @@ import java.time.Duration
 class CouchbaseRepository(
     connectionString: String,
     username: String,
-    password: String
+    password: String,
+    bucketName: String = "ProcessingStatus",
+    scopeName: String = "data",
+    reportsCollectionName: String = "Reports",
+    reportsDeadLetterCollectionName: String = "Reports-DeadLetter"
 ) : ProcessingStatusRepository() {
 
     // Connect without customizing the cluster environment
     private var cluster = Cluster.connect(connectionString, username, password)
 
-    private val processingStatusBucket = cluster.bucket("ProcessingStatus")
+    private val processingStatusBucket = cluster.bucket(bucketName)
 
     private val scope: Scope
 
@@ -43,14 +50,10 @@ class CouchbaseRepository(
 
     private val reportsDeadLetterCouchbaseCollection: com.couchbase.client.java.Collection
 
-    private val reportsCollectionName = "Reports"
-
-    private val reportsDeadLetterCollectionName = "Reports-DeadLetter"
-
     init {
         processingStatusBucket.waitUntilReady(Duration.ofSeconds(10))
 
-        scope = processingStatusBucket.scope("data")
+        scope = processingStatusBucket.scope(scopeName)
 
         reportsCouchbaseCollection = scope.collection(reportsCollectionName)
 
