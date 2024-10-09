627f4460d1d44abd80f281d5db39d721eb29a5c8
Matt B Krystof <mattkrystof@gmail.com>
2024-10-09 14:24:56 -0500

    Removing unused/obsolete function apps (#207)
    Co-authored-by: Matt B Krystof <ygj6@cdc.gov>

--- processing-status-api-function-app/src/main/kotlin/gov/cdc/ocio/processingstatusapi/model/reports/stagereports/HL7Validation.kt
+++
@@ -1,8 +0,0 @@
-package gov.cdc.ocio.processingstatusapi.model.reports.stagereports
-
-class HL7Validation {
-
-    companion object {
-        val schemaDefinition = SchemaDefinition(schemaName = "DEX HL7v2 STRUCTURE-VALIDATOR", schemaVersion = "0.0.1")
-    }
-}
--- processing-status-cicd-poc-app/src/main/kotlin/gov/cdc/ocio/processingstatusapi/exceptions/BadRequestException.kt
+++
@@ -1,9 +0,0 @@
-package gov.cdc.ocio.processingstatusapi.exceptions
-
-/**
- * Intended use of this exception is for bad requests, such as a record could not be located because an invalid
- * identifier was provided.  Or, if a required parameter is missing for a request.
- *
- * @constructor
- */
-class BadRequestException(message: String): Exception(message)
