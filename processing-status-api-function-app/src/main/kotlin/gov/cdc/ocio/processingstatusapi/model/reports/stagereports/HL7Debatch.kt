c8f50427e82a33214b09636c3a5101a607e6d7aa
Matt B Krystof <ygj6@cdc.gov>
2024-10-09 14:12:40 -0500

    Removing unused/obsolete function apps

--- processing-status-api-function-app/src/main/kotlin/gov/cdc/ocio/processingstatusapi/model/reports/stagereports/HL7Debatch.kt
+++
@@ -1,8 +0,0 @@
-package gov.cdc.ocio.processingstatusapi.model.reports.stagereports
-
-class HL7Debatch {
-
-    companion object {
-        val schemaDefinition = SchemaDefinition(schemaName = "DEX HL7v2 RECEIVER", schemaVersion = "2.0.0")
-    }
-}
