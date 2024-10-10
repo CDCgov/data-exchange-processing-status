f50d2303ae6ca5268c90d9f7153be9afbe87d782
VZE3 <vze3@cdc.gov>
2024-10-10 10:33:55 -0400

    Rename project

---
+++ pstatus-event-reader-sink/src/main/kotlin/gov/cdc/ocio/reader/sink/model/CloudConfig.kt
@@ -0,0 +1,25 @@
+package gov.cdc.ocio.reader.sink.model
+
+data class CloudConfig(
+    var provider: String, // "aws" or "azure"
+
+    //AWS Config Values
+    var awsAccessKeyId: String? =null,
+    var awsSecretAccessKey: String? =null,
+    var awsSqsQueueName: String? = null,
+    var awsSqsQueueURL: String? = null,
+    var awsSqsRegion: String? = null,
+    var awsS3BucketName: String? = null,
+    var awsS3Region: String? = null,
+
+    //Azure Config Values
+    var namespace: String? = null,
+    var connectionString: String? = null,
+    var sharedAccessKeyName: String? = null,
+    var sharedAccessKey: String? = null,
+    var topicName: String? = null,
+    var subscriptionName: String? = null,
+    var containerName: String? = null,
+    var storageAccountKey: String? = null,
+    var storageAccountName: String? = null
+)
