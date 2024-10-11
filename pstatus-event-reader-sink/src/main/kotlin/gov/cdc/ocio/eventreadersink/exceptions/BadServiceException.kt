9b7e3ccdfbf576a31177785a337e51187c2bf45f
VZE3 <vze3@cdc.gov>
2024-10-11 04:46:53 -0400

    Event-Reader-Sink updates

--- pstatus-event-reader-sink/src/main/kotlin/gov/cdc/ocio/eventreadersink/exceptions/BadServiceException.kt
+++
@@ -1,8 +0,0 @@
-package gov.cdc.ocio.eventreadersink.exceptions
-
-/**
- * Intended use of this exception is for bad requests, or, if a required parameter is missing for a request.
- *
- * @constructor
- */
-class BadServiceException(message: String): Exception(message)
