12dcd06e1afe48204c66e3c285b468ee3279ee92
candace-campbell <ccampbell@fearless.tech>
2024-11-25 12:25:39 -0500

    Removed aws credentials

--- libs/commons-database/src/main/kotlin/gov/cdc/ocio/database/health/HealthCheckDynamoDb.kt
+++ libs/commons-database/src/main/kotlin/gov/cdc/ocio/database/health/HealthCheckDynamoDb.kt
@@ -3,6 +3,8 @@ package gov.cdc.ocio.database.health
 import gov.cdc.ocio.types.health.HealthCheckSystem
 import gov.cdc.ocio.types.health.HealthStatusType
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.auth.credentials.WebIdentityTokenFileCredentialsProvider
 
 
 /**
@@ -10,9 +12,19 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbClient
  */
 class HealthCheckDynamoDb: HealthCheckSystem("Dynamo DB") {
 
+    private val awsCredentialProvider: WebIdentityTokenFileCredentialsProvider by lazy {
+        WebIdentityTokenFileCredentialsProvider.build()
+        .roleArn(System.getenv("AWS_ROLE_ARN))
+        .webIdentityTokenFile(System.getenv("AWS_WEB_IDENTITY_TOKEN_FILE"))
+    }
+
     // Lazily initialized DynamoDB client
     private val dynamoDbClient: DynamoDbClient by lazy {
-        DynamoDbClient.builder().build()
+        DynamoDbClient.builder()
+        .region(Region.US_EAST_1)
+        .build()
+        .credentialsProvider(awsCredentialProvider)
+
     }
     /**
      * Perform the dynamodb health check operations.
