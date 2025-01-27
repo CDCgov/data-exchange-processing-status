ff3b7e6248d9cf69de70261e7006d20dd927a55b
Matt B Krystof <ygj6@cdc.gov>
2025-01-27 14:11:42 -0600

    Changed routing to return a 500 for the health checks if the status is not UP

--- pstatus-report-sink-ktor/src/main/kotlin/gov/cdc/ocio/processingstatusapi/plugins/Routing.kt
+++ pstatus-report-sink-ktor/src/main/kotlin/gov/cdc/ocio/processingstatusapi/plugins/Routing.kt
@@ -1,9 +1,12 @@
 package gov.cdc.ocio.processingstatusapi.plugins
 
 import gov.cdc.ocio.processingstatusapi.health.HealthQueryService
+import gov.cdc.ocio.types.health.HealthStatusType
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+
 
 /**
  * Configures the REST routing endpoints for the application.
@@ -14,7 +17,12 @@ fun Application.configureRouting() {
     val version = environment.config.propertyOrNull("ktor.version")?.getString() ?: "unknown"
     routing {
         get("/health") {
-            call.respond(HealthQueryService().getHealth())
+            val result = HealthQueryService().getHealth()
+            val responseCode = when (result.status) {
+                HealthStatusType.STATUS_UP -> HttpStatusCode.OK
+                else -> HttpStatusCode.InternalServerError
+            }
+            call.respond(responseCode, result)
         }
         get("/version") {
             call.respondText(version)
