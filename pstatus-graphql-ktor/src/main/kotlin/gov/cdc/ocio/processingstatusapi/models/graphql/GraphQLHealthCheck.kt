3e5b4c50491b3025907a82bcf186748f585d61c8
Matt B Krystof <ygj6@cdc.gov>
2025-01-27 14:13:20 -0600

    Updated GraphQL health check type to use HealthStatusType instead of string for status

--- pstatus-graphql-ktor/src/main/kotlin/gov/cdc/ocio/processingstatusapi/models/graphql/GraphQLHealthCheck.kt
+++ pstatus-graphql-ktor/src/main/kotlin/gov/cdc/ocio/processingstatusapi/models/graphql/GraphQLHealthCheck.kt
@@ -1,19 +1,21 @@
 package gov.cdc.ocio.processingstatusapi.models.graphql
 
 import com.expediagroup.graphql.generator.annotations.GraphQLDescription
+import gov.cdc.ocio.types.health.HealthStatusType
+
 
 /**
  * Run health checks for the service.
  *
- * @property status String?
- * @property totalChecksDuration String?
- * @property dependencyHealthChecks MutableList<HealthCheckSystem>
+ * @property status [HealthStatusType]?
+ * @property totalChecksDuration [String]?
+ * @property dependencyHealthChecks [MutableList]<[GraphQLHealthCheckResult]>
  */
 @GraphQLDescription("Run health checks for the service")
 class GraphQLHealthCheck {
 
     @GraphQLDescription("Overall status of the service")
-    var status : String? = "DOWN"
+    var status : HealthStatusType? = HealthStatusType.STATUS_DOWN
 
     @GraphQLDescription("Total time it took to evaluate the health of the service and its dependencies")
     var totalChecksDuration : String? = null
