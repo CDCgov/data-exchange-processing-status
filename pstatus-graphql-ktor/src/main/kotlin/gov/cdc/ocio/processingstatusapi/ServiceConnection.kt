9ea80273d236f5c77ebd05b6d2b202c6b1f58ec1
Matt B Krystof <ygj6@cdc.gov>
2025-03-04 16:23:45 -0600

    Added missing function comment

--- pstatus-graphql-ktor/src/main/kotlin/gov/cdc/ocio/processingstatusapi/ServiceConnection.kt
+++ pstatus-graphql-ktor/src/main/kotlin/gov/cdc/ocio/processingstatusapi/ServiceConnection.kt
@@ -6,6 +6,16 @@ import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.logging.*
 import io.ktor.serialization.kotlinx.json.*
 
+
+/**
+ * A service connection is an HTTP client to a service that provides a REST API.
+ *
+ * @property serviceUrl String?
+ * @property unspecifiedUrl String
+ * @property serviceUnavailable String
+ * @property client HttpClient
+ * @constructor
+ */
 class ServiceConnection(
     serviceDescription: String,
     private val serviceUrl: String?
@@ -32,6 +42,12 @@ class ServiceConnection(
         }
     }
 
+    /**
+     * Builds a URL with the base service URL and path provided.
+     *
+     * @param path String
+     * @return String
+     */
     fun getUrl(path: String): String {
         if (serviceUrl.isNullOrBlank())
             throw Exception(unspecifiedUrl)
