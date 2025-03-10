eb48c7e50b7d30449627a680e2b747bdb150d63c
Alex de los Reyes <areyes@fearless.tech>
2025-03-10 18:22:10 -0400

    Adding tests for getReport and data generator updates

--- test/playwright/fixtures/dataGenerator.ts
+++ test/playwright/fixtures/dataGenerator.ts
@@ -1,5 +1,4 @@
 import { faker } from "@faker-js/faker"
-import { start } from "repl"
 
 const baseReport = {
     report_schema_version: "1.0.0",
@@ -17,15 +16,6 @@ const baseReport = {
         aggregation: "SINGLE",
         message_index: 1
     },
-    stage_info: {
-        service: "UPLOAD API",
-        action: "upload-completed",
-        version: "0.0.49-SNAPSHOT",
-        status: "SUCCESS",
-        issues: null,
-        start_processing_time: "2024-07-10T15:40:10.162+00:00",
-        end_processing_time: "2024-07-10T15:40:10.228+00:00"
-    },
     tags: { tag_field1: "value1" },
     data: { data_field1: "value1" },
     content_type: "application/json",
@@ -39,13 +29,13 @@ const baseReport = {
 
 const minimalReport = {
     upload_id: "uuid",
-        data_stream_id: "dextesting",
-        data_stream_route: "testevent1",
-        content_type: "application/json",
-        content: {
-            content_schema_name: "upload-completed",
-            content_schema_version: "1.0.0",
-            status: "SUCCESS"
+    data_stream_id: "dextesting",
+    data_stream_route: "testevent1",
+    content_type: "application/json",
+    content: {
+        content_schema_name: "upload-completed",
+        content_schema_version: "1.0.0",
+        status: "SUCCESS"
     }
 }
 
@@ -60,13 +50,55 @@ export function createMinimalReport() {
 export function createUploadReport() {
     const dexIngestDateTime = randomTime(new Date())
 
-    let newReport = { ...baseReport }
+    let newReport = {
+        ...baseReport,
+        stage_info: createStageInfo(dexIngestDateTime)
+    }
     newReport.upload_id = faker.string.uuid()
     newReport.message_metadata.message_uuid = faker.string.uuid()
     newReport.dex_ingest_datetime = getFormattedDate(dexIngestDateTime)
     newReport.stage_info.start_processing_time = getFormattedDate(addSeconds(dexIngestDateTime, 10))
     newReport.stage_info.end_processing_time = getFormattedDate(addSeconds(dexIngestDateTime, 20))
     return newReport
+}
+
+export function createStageInfo(date: Date = new Date()) {
+    const stage_info =  {
+        service: "UPLOAD API",
+        action: "upload-completed",
+        version: "0.0.49-SNAPSHOT",
+        status: "SUCCESS",
+        start_processing_time: getFormattedDate(addSeconds(date, 10)),
+        end_processing_time: getFormattedDate(addSeconds(date, 20))
+    }
+
+    return stage_info
+}
+
+export function createStageInfoWithWarning(date: Date = new Date()) {
+    const stage_info_warn = {
+        ...createStageInfo(date),
+        issues: [
+            {
+                level: "WARNING",
+                message: "Warning message"
+            }
+        ]
+    }
+    return stage_info_warn
+}
+
+export function createStageInfoWithError(date: Date = new Date()) {
+    const stage_info_error = {
+        ...createStageInfo(date),
+        issues: [
+            {
+                level: "ERROR",
+                message: "Error message"
+            }
+        ]
+    }
+    return stage_info_error
 }
 
 function getFormattedDate(date: Date = new Date()): string {
--- test/playwright/tests/getReport.spec.ts
+++ test/playwright/tests/getReport.spec.ts
@@ -1,13 +1,11 @@
 import { test, expect } from '@fixtures/gql';
 import { SortOrder } from '@gql';
-import { createUploadReport } from 'fixtures/dataGenerator';
-import { report } from 'process';
+import { createMinimalReport, createStageInfoWithError, createStageInfoWithWarning, createUploadReport } from 'fixtures/dataGenerator';
 
 test.describe('GraphQL getReports', () => {
-    let uploadReport: any
 
-    test.beforeAll(async ({ gql }) => {
-        uploadReport = await createUploadReport();
+    test('returns a report for a typical upload report', async ({ gql }) => {
+        const uploadReport = await createUploadReport();
         console.log(uploadReport)
 
         const createReportResult = await gql.upsertReport({
@@ -16,10 +14,7 @@ test.describe('GraphQL getReports', () => {
         })
         console.log(createReportResult)
         expect(createReportResult.upsertReport.result).toBe("SUCCESS")
-        
-    })
-    
-    test('returns a report for a known upload id', async ({ gql }) => {
+
         const reportResult = await gql.getReports({
             uploadId: uploadReport.upload_id,
             reportsSortedBy: "timestamp",
@@ -30,7 +25,7 @@ test.describe('GraphQL getReports', () => {
 
         await reportResult.getReports.forEach(report => {
             expect(report.reportSchemaVersion).toEqual(uploadReport.report_schema_version)
-            expect(report.uploadId).toEqual(uploadReport.upload_id)            
+            expect(report.uploadId).toEqual(uploadReport.upload_id)
             expect(report.dataStreamId).toEqual(uploadReport.data_stream_id)
             expect(report.dataStreamRoute).toEqual(uploadReport.data_stream_route)
             expect(report.jurisdiction).toEqual(uploadReport.jurisdiction)
@@ -38,6 +33,7 @@ test.describe('GraphQL getReports', () => {
             expect(report.dataProducerId).toEqual(uploadReport.data_producer_id)
             expect(report.dataProducerId).toEqual(uploadReport.data_producer_id)
             expect(report.dataProducerId).toEqual(uploadReport.data_producer_id)
+            expect(report.contentType).toEqual(uploadReport.content_type)
             expect(report.id).toBeDefined()
             expect(report.timestamp).toBeDefined()
             //expect(report.dexIngestDateTime).toEqual(uploadReport.dex_ingest_datetime)
@@ -58,13 +54,134 @@ test.describe('GraphQL getReports', () => {
             
             //stage info section
             expect(report.stageInfo?.status).toEqual(uploadReport.stage_info.status)
-            expect(report.stageInfo?.issues).toEqual(uploadReport.stage_info.issues)
+            expect(report.stageInfo?.issues).toBeNull()
             // expect(report.stageInfo?.startProcessingTime).toEqual(uploadReport.stage_info.start_processing_time)
             // expect(report.stageInfo?.endProcessingTime).toEqual(uploadReport.end_processing_time)
             
             //tags section
             expect(report.tags.tagField1).toEqual(uploadReport.tags.tag_field1)
         });
-    })
+    });
 
-} )
+    test('returns a report for a minimal upload report', async ({ gql }) => {
+        const uploadReport = await createMinimalReport();
+        console.log(uploadReport)
+
+        const createReportResult = await gql.upsertReport({
+            action: "create",
+            report: uploadReport
+        })
+        console.log(createReportResult)
+        expect(createReportResult.upsertReport.result).toBe("SUCCESS")
+
+
+        const reportResult = await gql.getReports({
+            uploadId: uploadReport.upload_id,
+            reportsSortedBy: "timestamp",
+            sortOrder: SortOrder.Ascending
+        })
+        console.log(reportResult.getReports[0])
+        expect(reportResult.getReports.length).toBeGreaterThanOrEqual(1)
+        await reportResult.getReports.forEach(report => {
+            expect(report.uploadId).toEqual(uploadReport.upload_id)
+            expect(report.dataStreamId).toEqual(uploadReport.data_stream_id)
+            expect(report.dataStreamRoute).toEqual(uploadReport.data_stream_route)
+            expect(report.content.contentSchemaName).toEqual(uploadReport.content.content_schema_name)
+            expect(report.content.contentSchemaVersion).toEqual(uploadReport.content.content_schema_version)
+            expect(report.content.status).toEqual(uploadReport.content.status)
+            expect(report.contentType).toEqual(uploadReport.content_type)
+        });
+
+    });
+
+    test('returns a report for a minimal upload report with error issue', async ({ gql }) => {
+        const uploadReport = {
+            ...createMinimalReport(),
+            stage_info: createStageInfoWithError()
+        }
+
+        const createReportResult = await gql.upsertReport({
+            action: "create",
+            report: uploadReport
+        })
+        expect(createReportResult.upsertReport.result).toBe("SUCCESS")
+
+        const reportResult = await gql.getReports({
+            uploadId: uploadReport.upload_id,
+            reportsSortedBy: "timestamp",
+            sortOrder: SortOrder.Ascending
+        })
+
+        expect(reportResult.getReports.length).toBeGreaterThanOrEqual(1)
+        await reportResult.getReports.forEach(report => {
+            expect(report.uploadId).toEqual(uploadReport.upload_id)
+            expect(report.dataStreamId).toEqual(uploadReport.data_stream_id)
+            expect(report.dataStreamRoute).toEqual(uploadReport.data_stream_route)
+            expect(report.content.contentSchemaName).toEqual(uploadReport.content.content_schema_name)
+            expect(report.content.contentSchemaVersion).toEqual(uploadReport.content.content_schema_version)
+            expect(report.content.status).toEqual(uploadReport.content.status)
+            expect(report.contentType).toEqual(uploadReport.content_type)
+
+            //stage info section
+            expect(report.stageInfo?.status).toEqual(uploadReport.stage_info.status)
+            expect(report.stageInfo?.issues).toEqual(uploadReport.stage_info.issues)
+            // expect(report.stageInfo?.startProcessingTime).toEqual(uploadReport.stage_info.start_processing_time)
+            // expect(report.stageInfo?.endProcessingTime).toEqual(uploadReport.end_processing_time)
+           
+            expect(report.stageInfo?.issues).toHaveLength(1)
+            report.stageInfo?.issues?.forEach((issue, index) => {
+                console.log("Recieved issue:", issue)
+                expect(issue.level).toEqual(uploadReport.stage_info.issues[index].level)
+                expect(issue.message).toEqual(uploadReport.stage_info.issues[index].message)
+            })
+        });
+        
+    });
+
+    test('returns a report for a minimal upload report with warn issue', async ({ gql }) => {
+        const uploadReport = {
+            ...createMinimalReport(),
+            stage_info: createStageInfoWithWarning()
+        }
+
+        const createReportResult = await gql.upsertReport({
+            action: "create",
+            report: uploadReport
+        })
+        expect(createReportResult.upsertReport.result).toBe("SUCCESS")
+
+
+        const reportResult = await gql.getReports({
+            uploadId: uploadReport.upload_id,
+            reportsSortedBy: "timestamp",
+            sortOrder: SortOrder.Ascending
+        })
+
+        expect(reportResult.getReports.length).toBeGreaterThanOrEqual(1)
+        await reportResult.getReports.forEach(report => {
+            expect(report.uploadId).toEqual(uploadReport.upload_id)
+            expect(report.dataStreamId).toEqual(uploadReport.data_stream_id)
+            expect(report.dataStreamRoute).toEqual(uploadReport.data_stream_route)
+            expect(report.content.contentSchemaName).toEqual(uploadReport.content.content_schema_name)
+            expect(report.content.contentSchemaVersion).toEqual(uploadReport.content.content_schema_version)
+            expect(report.content.status).toEqual(uploadReport.content.status)
+            expect(report.contentType).toEqual(uploadReport.content_type)
+
+            //stage info section
+            expect(report.stageInfo?.status).toEqual(uploadReport.stage_info.status)
+            expect(report.stageInfo?.issues).toEqual(uploadReport.stage_info.issues)
+            // expect(report.stageInfo?.startProcessingTime).toEqual(uploadReport.stage_info.start_processing_time)
+            // expect(report.stageInfo?.endProcessingTime).toEqual(uploadReport.end_processing_time)
+           
+            expect(report.stageInfo?.issues).toHaveLength(1)
+            report.stageInfo?.issues?.forEach((issue, index) => {
+                console.log("Recieved issue:", issue)
+                expect(issue.level).toEqual(uploadReport.stage_info.issues[index].level)
+                expect(issue.message).toEqual(uploadReport.stage_info.issues[index].message)
+            })
+        });
+        
+    });
+
+});
+
