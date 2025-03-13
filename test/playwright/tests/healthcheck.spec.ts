be47f86852ab2df8a6557a319a4e37a764be0de9
Alex de los Reyes <cyberdecker@gmail.com>
2025-03-13 13:46:10 -0400

    Adding notifications environment urls and renaming snapshot files

--- test/playwright/tests/healthcheck.spec.ts
+++ test/playwright/tests/healthcheck.spec.ts
@@ -1,29 +1,31 @@
-import { test, expect, request} from '@playwright/test';
-import { Environments } from 'fixtures/environment';
+import { test, expect, request } from "@playwright/test";
+import { Environments } from "fixtures/environment";
 
 let testEnvironments = new Environments(process.env.ENV?.toString());
-test.describe('Healthcheck', () => {
-    testEnvironments.envs.forEach(({ environment, name, url }) => {
-        test(`[${environment}] ${name}@${url}`, async ({ request }) => {
-            const healthRequest = await request.get(`${url}/health`)
-            expect(await healthRequest.ok()).toBeTruthy()
-            const healthResponse = await healthRequest.json()
-            expect(JSON.stringify(healthResponse.dependencyHealthChecks)).toMatchSnapshot("healthcheck.json")
-        })
-    })
-})
+test.describe("Healthcheck", () => {
+  testEnvironments.envs.forEach(({ environment, name, url }) => {
+    test(`[${environment}] ${name}`, async ({ request }) => {
+      const healthRequest = await request.get(`${url}/health`);
+      expect(await healthRequest.status()).toBe(200);
+      const healthResponse = await healthRequest.json();
+      expect(
+        JSON.stringify(healthResponse.dependencyHealthChecks)
+      ).toMatchSnapshot("healthcheck.json");
+    });
+  });
+});
 
-test.describe('Version', () => {
-    testEnvironments.envs.forEach(({ environment, name, url }) => {
-        test(`[${environment}] ${name}@${url}`, async ({ request }) => {
-            const version = await request.get(`${url}/version`)
-            expect(await version.ok()).toBeTruthy()
-            const versionResponse = await version.json()
-            expect(versionResponse.version).toBeDefined()
-            expect(versionResponse.branch).toBeDefined()
-            expect(versionResponse.commit).toBeDefined()
-            expect(versionResponse.commitId).toBeDefined()
-            expect(versionResponse.commitTime).toBeDefined()
-        })
-    })
-})
+test.describe("Version", () => {
+  testEnvironments.envs.forEach(({ environment, name, url }) => {
+    test(`[${environment}] ${name}`, async ({ request }) => {
+      const version = await request.get(`${url}/version`);
+      expect(await version.status()).toBe(200);
+      const versionResponse = await version.json();
+      expect(versionResponse.version).toBeDefined();
+      expect(versionResponse.branch).toBeDefined();
+      expect(versionResponse.commit).toBeDefined();
+      expect(versionResponse.commitId).toBeDefined();
+      expect(versionResponse.commitTime).toBeDefined();
+    });
+  });
+});
