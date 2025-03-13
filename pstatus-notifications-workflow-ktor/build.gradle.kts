49fc6ca8c79968f3a519bae9eca7f0744d25dd9e
Matt B Krystof <ygj6@cdc.gov>
2025-03-13 16:42:44 -0500

    Removed unneeded libs from dependencies of workflow

--- pstatus-notifications-workflow-ktor/build.gradle.kts
+++ pstatus-notifications-workflow-ktor/build.gradle.kts
@@ -65,12 +65,6 @@ dependencies {
     implementation ("io.netty:netty-tcnative-boringssl-static:2.0.52.Final:windows-x86_64")
     implementation ("software.amazon.awssdk:sts:2.29.34")
     implementation ("com.cronutils:cron-utils:9.2.1")
-
-    // include for JVM target
-//    implementation ("org.jetbrains.kotlinx:kotlinx-html-jvm:$kotlinxHtmlVersion")
-    // include for JS target
-//    implementation ("org.jetbrains.kotlinx:kotlinx-html-js:$kotlinxHtmlVersion")
-    // include for Common module
     implementation ("org.jetbrains.kotlinx:kotlinx-html:$kotlinxHtmlVersion")
 
     testImplementation(kotlin("test"))
