89a69cfcd81f8d2a2f369897e7d8d917a6e83585
Matt B Krystof <ygj6@cdc.gov>
2025-03-14 15:16:58 -0500

    Added InstantRange to common types lib

--- libs/commons-types/src/main/kotlin/gov/cdc/ocio/types/InstantRange.kt
+++ libs/commons-types/src/main/kotlin/gov/cdc/ocio/types/InstantRange.kt
@@ -1,4 +1,33 @@
 package gov.cdc.ocio.types
 
-class types {
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneOffset
+
+
+/**
+ * Used to specify a date range as instants.
+ *
+ * @property start Instant
+ * @property endInclusive Instant
+ * @constructor
+ */
+class InstantRange(override val start: Instant, override val endInclusive: Instant)
+    : ClosedRange<Instant> {
+
+    companion object {
+
+        /**
+         * Creates a [InstantRange] from the provided [LocalDate], which will be assumed to be in the UTC timezone.
+         *
+         * @param date LocalDate
+         * @return InstantRange
+         */
+        fun fromLocalDate(date: LocalDate): InstantRange {
+            val startOfDay = date.atStartOfDay(ZoneOffset.UTC).toInstant() // 00:00:00 UTC
+            val endOfDay = date.plusDays(1).atStartOfDay(ZoneOffset.UTC).toInstant() // 00:00:00 of next day UTC (exclusive)
+
+            return InstantRange(startOfDay, endOfDay)
+        }
+    }
 }
