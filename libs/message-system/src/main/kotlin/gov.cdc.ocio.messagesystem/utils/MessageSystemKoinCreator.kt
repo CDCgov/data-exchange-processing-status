6ae6dc47c9843f16dcc9aa9ec4d60a988379a590
Matt B Krystof <ygj6@cdc.gov>
2025-03-20 17:25:10 -0500

    Code cleanup of message system koin creator

--- libs/message-system/src/main/kotlin/gov.cdc.ocio.messagesystem/utils/MessageSystemKoinCreator.kt
+++ libs/message-system/src/main/kotlin/gov.cdc.ocio.messagesystem/utils/MessageSystemKoinCreator.kt
@@ -29,8 +29,7 @@ object MessageSystemKoinCreator {
     private fun createMessageSystem(
         environment: ApplicationEnvironment
     ): MessageSystem {
-        val messageSystemType = MessageSystemType.getFromAppEnv(environment)
-        return when (messageSystemType) {
+        return when (MessageSystemType.getFromAppEnv(environment)) {
             MessageSystemType.AZURE_SERVICE_BUS -> {
                 val config = AzureServiceBusConfiguration(environment.config, configurationPath = "azure.service_bus")
                 AzureServiceBusMessageSystem(config)
@@ -43,9 +42,7 @@ object MessageSystemKoinCreator {
                 val config = AWSSQSServiceConfiguration(environment.config, configurationPath = "aws")
                 AWSSQSMessageSystem(config.createSQSClient(), config.listenQueueURL, config.sendQueueURL)
             }
-            else -> {
-                UnsupportedMessageSystem(environment.config.tryGetString("ktor.message_system"))
-            }
+            else -> { UnsupportedMessageSystem(environment.config.tryGetString("ktor.message_system")) }
         }
     }
 
@@ -57,9 +54,7 @@ object MessageSystemKoinCreator {
      */
     fun moduleFromAppEnv(environment: ApplicationEnvironment): Module {
         return module {
-            single {
-                createMessageSystem(environment)
-            }
+            single { createMessageSystem(environment) }
         }
     }
 }
