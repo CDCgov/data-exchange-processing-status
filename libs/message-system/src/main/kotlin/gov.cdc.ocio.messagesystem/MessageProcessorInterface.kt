a238e901943dbdade9b9c9bd44e7d67276684808
Matt B Krystof <ygj6@cdc.gov>
2025-03-21 14:54:16 -0500

    Update the message processor interface to be a function interface

--- libs/message-system/src/main/kotlin/gov.cdc.ocio.messagesystem/MessageProcessorInterface.kt
+++ libs/message-system/src/main/kotlin/gov.cdc.ocio.messagesystem/MessageProcessorInterface.kt
@@ -3,7 +3,7 @@ package gov.cdc.ocio.messagesystem
 /**
  * Defines the common interfaces for all message processors.
  */
-interface MessageProcessorInterface {
+fun interface MessageProcessorInterface {
 
     /**
      * Process the provided message as a string.
