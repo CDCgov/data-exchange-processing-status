b468300560d5f2fcba1667d2f1c1b99f5f74e6c5
Matt B Krystof <ygj6@cdc.gov>
2025-03-21 15:01:23 -0500

    Added more properties to the report message in rules-engine

--- pstatus-notifications-rules-engine-ktor/src/main/kotlin/gov/cdc/ocio/processingstatusnotifications/model/message/ReportMessage.kt
+++ pstatus-notifications-rules-engine-ktor/src/main/kotlin/gov/cdc/ocio/processingstatusnotifications/model/message/ReportMessage.kt
@@ -3,7 +3,8 @@ package gov.cdc.ocio.processingstatusnotifications.model.message
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.google.gson.annotations.SerializedName
 import gov.cdc.ocio.messagesystem.models.DispositionType
-import java.util.*
+import java.time.Instant
+
 
 enum class Status {
 
@@ -14,30 +15,46 @@ enum class Status {
     FAILURE
 }
 
-data class StageInfo(
-    @JsonProperty("service")
-    var service : String? = null,
-    @JsonProperty("action")
-    var action: String? = null,
-    @JsonProperty("version")
-    var version: String? = null,
-    @JsonProperty("status")
-    var status: Status? = null,
-//    @JsonProperty("issues")
-//    var issues: List<Issue>? = null,
+/**
+ * Issue level of Report-ERROR OR WARNING
+ */
+enum class Level {
+    @SerializedName("ERROR")
+    ERROR,
+    @SerializedName("WARNING")
+    WARNING
+}
 
-//    @SerializedName("start_processing_time")
-//    @JsonProperty("start_processing_time")
-//    @JsonDeserialize(using = EpochToInstantConverter::class)
-//    var startProcessingTime:  Instant? = null,
-//    @SerializedName("end_processing_time")
-//    @JsonProperty("end_processing_time")
-//    @JsonDeserialize(using = EpochToInstantConverter::class)
-//    var endProcessingTime:  Instant? = null
-)
+class Issue {
+
+    var level : Level? = null
+
+    var message: String? = null
+}
+
+class StageInfo {
+
+    var service : String? = null
+
+    var action: String? = null
+
+    var version: String? = null
+
+    var status: Status? = null
+
+    var issues: List<Issue>? = null
+
+    @SerializedName("start_processing_time")
+    @JsonProperty("start_processing_time")
+    var startProcessingTime: Instant? = null
+
+    @SerializedName("end_processing_time")
+    @JsonProperty("end_processing_time")
+    var endProcessingTime: Instant? = null
+}
 
 /**
- * Incoming report definition.
+ * Incoming report message.
  *
  * @property uploadId String?
  * @property dataStreamId String?
@@ -45,8 +62,6 @@ data class StageInfo(
  * @property dispositionType DispositionType?
  * @property contentType String?
  * @property stageInfo StageInfo?
- * @property content Any?
- * @property contentAsString String?
  */
 class ReportMessage {
 
@@ -67,8 +82,4 @@ class ReportMessage {
 
     @SerializedName("stage_info")
     val stageInfo: StageInfo? = null
-
-    // content will vary depending on content_type so make it any.  For example, if content_type is json then the
-    // content type will be a Map<*, *>.
-    val content: Any? = null
 }
