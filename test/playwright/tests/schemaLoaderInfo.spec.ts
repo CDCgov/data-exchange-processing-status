cf842a830c40c1a9b5dca51b28551cded1be91eb
Alex de los Reyes <areyes@fearless.tech>
2025-04-01 11:22:59 -0400

    Minor test title change

--- test/playwright/tests/schemaLoaderInfo.spec.ts
+++ test/playwright/tests/schemaLoaderInfo.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@fixtures/gql';
 
-test.describe("schemaLoaderInfo Query", async () => {
+test.describe("schemaLoaderInfo query", async () => {
     test(`matches the expected snapshot for enviromnent: ${process.env.ENV}`, async ({ gql }, testInfo) => {
       const response = await gql.schemaLoaderInfo()
       expect(JSON.stringify(response.schemaLoaderInfo)).toMatchSnapshot("schemaLoaderInfo.json");
