5026fb413d3ada61b832a7c41ec92b4a3ad57bc1
Matt B Krystof <ygj6@cdc.gov>
2025-04-07 13:51:11 -0500

    Removed internal workflow status returned to callers getting workflow status

--- pstatus-notifications-workflow-ktor/src/main/kotlin/service/WorkflowStatusService.kt
+++ pstatus-notifications-workflow-ktor/src/main/kotlin/service/WorkflowStatusService.kt
@@ -1,6 +1,5 @@
 package gov.cdc.ocio.processingnotifications.service
 
-import gov.cdc.ocio.processingnotifications.model.WorkflowStatus
 import gov.cdc.ocio.processingnotifications.temporal.WorkflowEngine
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
@@ -13,9 +12,18 @@ class WorkflowStatusService : KoinComponent {
     /**
      * Get and return all the Temporal workflows.
      */
-    fun getAllWorkflows(): List<WorkflowStatus>  {
+    fun getAllWorkflows(): List<Map<String, Any>>  {
         try {
-            return workflowEngine.getAllWorkflows()
+            return workflowEngine.getAllWorkflows().map {
+                // Only provide a subset of the workflow status to callers as the other data is superfluous.
+                mapOf(
+                    "workflowId" to it.workflowId,
+                    "taskName" to it.taskName,
+                    "description" to it.description,
+                    "status" to it.status,
+                    "schedule" to it.schedule
+                )
+            }
         } catch (e: Exception) {
             throw Exception("Error occurred while checking for workflows: ${e.message}")
         }
