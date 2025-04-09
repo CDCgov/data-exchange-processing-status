261ad2210f27d7981c2a570424398e319ab15ddf
Matt B Krystof <ygj6@cdc.gov>
2025-04-09 12:18:28 -0500

    Updated the name of the upload metrics class

--- pstatus-notifications-workflow-ktor/src/main/kotlin/workflow/digestcounts/UploadDigestCountsNotificationWorkflowImpl.kt
+++ pstatus-notifications-workflow-ktor/src/main/kotlin/workflow/digestcounts/UploadDigestCountsNotificationWorkflowImpl.kt
@@ -4,7 +4,7 @@ import gov.cdc.ocio.database.persistence.ProcessingStatusRepository
 import gov.cdc.ocio.processingnotifications.activity.NotificationActivities
 import gov.cdc.ocio.processingnotifications.model.UploadDigestResponse
 import gov.cdc.ocio.processingnotifications.query.UploadDigestCountsQuery
-import gov.cdc.ocio.processingnotifications.query.UploadTimeDeltaQuery
+import gov.cdc.ocio.processingnotifications.query.UploadMetricsQuery
 import io.temporal.activity.ActivityOptions
 import io.temporal.common.RetryOptions
 import io.temporal.workflow.Workflow
@@ -69,8 +69,8 @@ class UploadDigestCountsNotificationWorkflowImpl :
             // Aggregate the upload counts
             val aggregatedCounts = aggregateUploadCounts(uploadDigestResults)
 
-            // Get the upload timing metrics
-            val uploadMetrics = UploadTimeDeltaQuery(repository)
+            // Get the upload metrics
+            val uploadMetrics = UploadMetricsQuery(repository)
                 .run(utcDateToRun, dataStreamIds, dataStreamRoutes, jurisdictions)
 
             // Format the email body
