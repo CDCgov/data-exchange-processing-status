8f9241a759ff1b02dedae2adeb28e356e9e444cf
Matt B Krystof <ygj6@cdc.gov>
2025-04-10 20:10:53 -0500

    Updated duration human readable formatting

--- libs/commons-types/src/main/kotlin/gov/cdc/ocio/types/extensions/DurationExtensions.kt
+++ libs/commons-types/src/main/kotlin/gov/cdc/ocio/types/extensions/DurationExtensions.kt
@@ -16,11 +16,10 @@ fun Duration.toHumanReadable(): String {
     val minutes = (seconds % 3600) / 60
     val remainingSeconds = seconds % 60
     val remainingMilliseconds = milliseconds % 1000
-    val roundedMillis = Math.round(remainingMilliseconds / 100.0) / 10 // Round to tenths place
 
     return buildString {
         if (hours > 0) append("$hours hr ")
         if (minutes > 0) append("$minutes min ")
-        append("$remainingSeconds${if (roundedMillis > 0) ".${(roundedMillis * 10).toInt()}" else ""} sec")
+        append("%d.%03d sec".format(remainingSeconds, remainingMilliseconds))
     }.trim()
 }
