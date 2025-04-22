fff03e98e6e3d8ecf0e5962dc8a963a9c5d02521
Matt B Krystof <ygj6@cdc.gov>
2025-04-22 16:13:38 -0500

    Fixed grammar in comment header

--- libs/notification-dispatchers/src/main/kotlin/gov/cdc/ocio/notificationdispatchers/email/EmailProtocol.kt
+++ libs/notification-dispatchers/src/main/kotlin/gov/cdc/ocio/notificationdispatchers/email/EmailProtocol.kt
@@ -3,9 +3,8 @@ package gov.cdc.ocio.notificationdispatchers.email
 /**
  * Enumerate all the supported email protocols.
  *
- * Note: There are a few other mail protocols less common email protocols like Messaging Application Programming
- * Interface (MAPI) for sending emails used by Outlook and Exchange.  If any others are supported in the future
- * this enum will be expanded.
+ * Note: There are other less common mail protocols such as Messaging Application Programming Interface (MAPI)
+ * used by Outlook and Exchange.  If any others are supported in the future this enum will be expanded.
  *
  * @property value String
  * @constructor
