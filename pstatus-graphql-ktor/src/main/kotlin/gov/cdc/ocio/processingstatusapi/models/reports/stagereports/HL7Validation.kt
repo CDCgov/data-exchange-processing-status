95fb0fdbd4af5247d6d43ec137911f0a92d9e376
Matt B Krystof <mattkrystof@gmail.com>
2025-04-28 15:38:24 -0500

    Removed unused files from graphql service

--- pstatus-graphql-ktor/src/main/kotlin/gov/cdc/ocio/processingstatusapi/models/reports/stagereports/HL7Validation.kt
+++
@@ -1,8 +0,0 @@
-package gov.cdc.ocio.processingstatusapi.models.reports.stagereports
-
-class HL7Validation {
-
-    companion object {
-        val schemaDefinition = SchemaDefinition(schemaName = "DEX HL7v2 STRUCTURE-VALIDATOR", schemaVersion = "0.0.1")
-    }
-}
