f90e3355e43d582e699fb783cd7da7f05cc779ec
candace-campbell <ccampbell@fearless.tech>
2025-05-01 17:04:49 -0400

    Added environment variables to docker-compose.local

--- local-run.sh
+++ local-run.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-echo "Building service images ..."
+echo "Building images ..."
 
 cd pstatus-graphql-ktor && ./gradlew jibDockerBuild
 cd ../pstatus-report-sink-ktor && ./gradlew jibDockerBuild
 cd ../pstatus-notifications-workflow-ktor && ./gradlew jibDockerBuild
 cd ../pstatus-notifications-rules-engine-ktor && ./gradlew jibDockerBuild
+cd ../
 
+echo "Starting services ..."
+docker compose -f docker-compose.local.yml --env-file mock-email.env up -d
