3360279228a301ef813546dea0529967e6993c7f
Matt B Krystof <mattkrystof@gmail.com>
2025-05-13 11:47:07 -0500

    Added top errors request and response descriptions

--- pstatus-notifications-workflow-ktor/src/main/kotlin/workflow/toperrors/TopErrorsResponse.kt
+++ pstatus-notifications-workflow-ktor/src/main/kotlin/workflow/toperrors/TopErrorsResponse.kt
@@ -4,6 +4,21 @@ import gov.cdc.ocio.processingnotifications.activity.DataResponse
 import gov.cdc.ocio.processingnotifications.model.UploadInfo
 
 
+/**
+ * Represents the aggregated data and statistics of the most significant issues encountered
+ * in a data processing workflow over a specific time interval.
+ *
+ * This response provides details about counts and lists of various types of issues,
+ * such as metadata validation failures, failed deliveries, delayed uploads, delayed deliveries,
+ * and abandoned uploads. It is used to facilitate error reporting, monitoring, and troubleshooting
+ * workflows by providing actionable insights.
+ *
+ * @property failedMetadataValidationCount The total count of uploads that failed metadata validation.
+ * @property failedDeliveryCount The total count of uploads that failed during the delivery process.
+ * @property delayedUploads A list of uploads that were initiated but have been delayed for more than a specific duration.
+ * @property delayedDeliveries A list of uploads that were successfully completed but delayed in the delivery phase.
+ * @property abandonedUploads A list of uploads that were initiated but abandoned without completion for an extended period.
+ */
 data class TopErrorsResponse(
     val failedMetadataValidationCount: Int,
     val failedDeliveryCount: Int,
