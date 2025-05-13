8de5a25cd6911b7629157e0f1ae4067ede0efe72
Matt B Krystof <mattkrystof@gmail.com>
2025-05-13 11:56:55 -0500

    Code cleanup on top errors workflow activities implementation

--- pstatus-notifications-workflow-ktor/src/main/kotlin/workflow/toperrors/TopErrorsNotificationActivitiesImpl.kt
+++ pstatus-notifications-workflow-ktor/src/main/kotlin/workflow/toperrors/TopErrorsNotificationActivitiesImpl.kt
@@ -8,8 +8,30 @@ import gov.cdc.ocio.processingnotifications.activity.ResultWrapper
 import gov.cdc.ocio.processingnotifications.service.ReportService
 
 
+/**
+ * Implementation of the `NotificationActivitiesImpl` class that is responsible for
+ * collecting and processing data related to top error notifications for reports.
+ *
+ * This class interacts with `ReportService` to gather error counts and report details
+ * such as failed metadata verification, failed deliveries, delayed uploads, delayed
+ * deliveries, and abandoned uploads. The collected data is wrapped into a `TopErrorsResponse`
+ * and returned as a result.
+ */
 class TopErrorsNotificationActivitiesImpl : NotificationActivitiesImpl() {
 
+    private val reportService = ReportService()
+
+    /**
+     * Collects data regarding the most significant errors and issues within a specified
+     * time interval for a given data stream. This includes metrics like failed metadata verification,
+     * failed deliveries, delayed uploads, delayed deliveries, and abandoned uploads.
+     *
+     * @param request The data request of type [DataRequest].
+     *        It must be an instance of [TopErrorsRequest] containing the data stream ID,
+     *        route, and time interval for analysis.
+     * @return A [ResultWrapper] wrapping a [TopErrorsResponse] containing the error metrics
+     *         and insights for the given data stream and interval.
+     */
     override fun collectData(request: DataRequest): ResultWrapper<DataResponse> {
         val uploadDigestCountsRequest = request as TopErrorsRequest
 
@@ -18,12 +40,32 @@ class TopErrorsNotificationActivitiesImpl : NotificationActivitiesImpl() {
         val dayInterval = uploadDigestCountsRequest.dayInterval
 
         // Logic to determine the counts
-        val reportService = ReportService()
-        val failedMetadataVerifyCount = reportService.countFailedReports(dataStreamId, dataStreamRoute, StageAction.METADATA_VERIFY, dayInterval)
-        val failedDeliveryCount = reportService.countFailedReports(dataStreamId, dataStreamRoute, StageAction.FILE_DELIVERY, dayInterval)
-        val delayedUploads = reportService.getDelayedUploads(dataStreamId, dataStreamRoute, dayInterval)
-        val delayedDeliveries = reportService.getDelayedDeliveries(dataStreamId, dataStreamRoute, dayInterval)
-        val abandonedUploads = reportService.getAbandonedUploads(dataStreamId, dataStreamRoute)
+        val failedMetadataVerifyCount = reportService.countFailedReports(
+            dataStreamId,
+            dataStreamRoute,
+            StageAction.METADATA_VERIFY,
+            dayInterval
+        )
+        val failedDeliveryCount = reportService.countFailedReports(
+            dataStreamId,
+            dataStreamRoute,
+            StageAction.FILE_DELIVERY,
+            dayInterval
+        )
+        val delayedUploads = reportService.getDelayedUploads(
+            dataStreamId,
+            dataStreamRoute,
+            dayInterval
+        )
+        val delayedDeliveries = reportService.getDelayedDeliveries(
+            dataStreamId,
+            dataStreamRoute,
+            dayInterval
+        )
+        val abandonedUploads = reportService.getAbandonedUploads(
+            dataStreamId,
+            dataStreamRoute
+        )
 
         return ResultWrapper.Success(
             TopErrorsResponse(
