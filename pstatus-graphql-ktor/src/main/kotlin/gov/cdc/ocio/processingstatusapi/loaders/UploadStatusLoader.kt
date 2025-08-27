4197b3b2b24eb70eb88c62520557ca800d3474ee
Matt B Krystof <mattkrystof@gmail.com>
2025-08-27 17:23:26 -0500

    Fixes date range query unit conversion.
    Ensures correct unit conversion for start and end dates when querying by date range, converting milliseconds to seconds for accurate comparisons.

--- pstatus-graphql-ktor/src/main/kotlin/gov/cdc/ocio/processingstatusapi/loaders/UploadStatusLoader.kt
+++ pstatus-graphql-ktor/src/main/kotlin/gov/cdc/ocio/processingstatusapi/loaders/UploadStatusLoader.kt
@@ -81,12 +81,12 @@ class UploadStatusLoader: KoinComponent {
         }
 
         dateStart?.run {
-            val dateStartEpochMillis = timeFunc(DateUtils.getEpochFromDateString(dateStart))
-            sqlQuery.append(" and ${cPrefix}dexIngestDateTime >= $dateStartEpochMillis")
+            val dateStartEpochMillis = DateUtils.getEpochFromDateString(dateStart) / 1000
+            sqlQuery.append(" and ${cPrefix}dexIngestDateTime >= ${timeFunc(dateStartEpochMillis)}")
         }
         dateEnd?.run {
-            val dateEndEpochMillis = timeFunc(DateUtils.getEpochFromDateString(dateEnd))
-            sqlQuery.append(" and ${cPrefix}dexIngestDateTime < $dateEndEpochMillis")
+            val dateEndEpochMillis = DateUtils.getEpochFromDateString(dateEnd) / 1000
+            sqlQuery.append(" and ${cPrefix}dexIngestDateTime < ${timeFunc(dateEndEpochMillis)}")
         }
 
         sqlQuery.append(" group by ${cPrefix}uploadId, ${cPrefix}jurisdiction, ${cPrefix}senderId")
